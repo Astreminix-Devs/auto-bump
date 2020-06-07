e1ddb538b4145c2db12f24756446aa690ade9b8b
astreminix <66201263+astreminix@users.noreply.github.com>
2020-06-08 09:40:40 +1200

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+function sleep(ms) {
+return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+client.on('ready', async () => {
+
+var server = client.guilds.get("712773337864077382");
+var channel = server.channels.get("717115306446946406")
+
+        console.log(`Logged in as ${client.user.username}, ready to auto bump.`);
+        for (var i = 0; i < 1000000; i++) {
+       channel.send('!d bump')
+        await sleep(7200000)
+}
+ 
+    
+    });
+
+client.login(process.env.token)
